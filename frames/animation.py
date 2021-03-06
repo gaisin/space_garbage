30cb0667ca3db5fa7d53b5e7e95354af885bb9f7
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2021-03-07 01:49:07 +0300

    add func to animate garbage from top to bottom

--- frames/animation.py
+++ frames/animation.py
@@ -91,3 +91,21 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         row += rows_speed
         column += columns_speed
 
+
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    '''Animate garbage, flying from top to bottom.
+    Сolumn position will stay same, as specified on start.
+    '''
+
+    rows_number, columns_number = canvas.getmaxyx()  # getmaxyx returns heigh and width of window
+
+    column = max(column, 0)
+    column = min(column, columns_number - 1)
+
+    row = 0
+
+    while row < rows_number:
+        frames.common.draw_frame(canvas, row, column, garbage_frame)
+        await asyncio.sleep(0)
+        frames.common.draw_frame(canvas, row, column, garbage_frame, negative=True)
+        row += speed
